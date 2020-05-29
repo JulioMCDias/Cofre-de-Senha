8599456b98ff231d503fb3220066b54d84af43d2
JulioMCDias <juliomarques11@gmail.com>
2020-05-29 20:12:45 -0300

    init repository

---
+++ android/app/src/main/java/com/jlmdeveloper/cofresenha/MainActivity.java
@@ -0,0 +1,6 @@
+package com.jlmdeveloper.cofresenha;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ linux/window_configuration.cc
@@ -0,0 +1,5 @@
+#include "window_configuration.h"
+
+const char *kFlutterWindowTitle = "cofresenha";
+const unsigned int kFlutterWindowWidth = 450;
+const unsigned int kFlutterWindowHeight = 800;
